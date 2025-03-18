9275862180d48bb5fff77ae254e28e29e6c53d4e
Deepanshu Saini <70814952+Deepanshu09-max@users.noreply.github.com>
2025-03-18 19:05:34 +0530

    Update calculator.py

--- backend/calculator.py
+++ backend/calculator.py
@@ -9,6 +9,19 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           
+    allow_credentials=True,          
+    allow_methods=["*"],            
+    allow_headers=["*"],            
+)
+
 @app.get("/")
 def home():
     return FileResponse("static/index.html")
